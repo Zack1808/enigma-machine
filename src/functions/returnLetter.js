f86b8d70c162dc0e450aecaba95ab9ba6ee4150f
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2023-07-27 10:55:20 +0200

    Fixed the encoder function

--- src/functions/returnLetter.js
+++ src/functions/returnLetter.js
@@ -12,22 +12,22 @@ export const encript = (key, rotor1, rotor2, rotor3, pos1, pos2, pos3) => {
   if (!letters.includes(key.toUpperCase())) return key;
   var index = letters.indexOf(key.toUpperCase());
   index = letters.indexOf(
-    rotors[rotor1][index + pos1 <= 25 ? index + pos1 : index + pos1 - 25]
+    rotors[rotor1][index + pos1 <= 25 ? index + pos1 : index + pos1 - 26]
   );
   index = letters.indexOf(
-    rotors[rotor2][index + pos2 <= 25 ? index + pos2 : index + pos2 - 24]
+    rotors[rotor2][index + pos2 <= 25 ? index + pos2 : index + pos2 - 26]
   );
   index = letters.indexOf(
-    rotors[rotor3][index + pos3 <= 25 ? index + pos3 : index + pos3 - 23]
+    rotors[rotor3][index + pos3 <= 25 ? index + pos3 : index + pos3 - 26]
   );
   index = letters.indexOf(reflectorLetters[index]);
   index = reflectorLetters.indexOf(reflectorLetters[index]);
   index = rotors[rotor3].indexOf(letters[index]);
   index = rotors[rotor2].indexOf(
-    letters[index - pos3 >= 0 ? index - pos3 : index - pos3 + 23]
+    letters[index - pos3 >= 0 ? index - pos3 : index - pos3 + 26]
   );
   index = rotors[rotor1].indexOf(
-    letters[index - pos2 >= 0 ? index - pos2 : index - pos2 + 24]
+    letters[index - pos2 >= 0 ? index - pos2 : index - pos2 + 26]
   );
-  return letters[index - pos1 >= 0 ? index - pos1 : index - pos1 + 25];
+  return letters[index - pos1 >= 0 ? index - pos1 : index - pos1 + 26];
 };
